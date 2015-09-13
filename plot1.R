7e996a941f28bf47b5f47d267164ee3cef4b2e19
amalgamatedclyde <clyde@amalgamated-clyde.com>
2015-09-13 11:56:38 -0400

    specify path to data

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+# setwd("~/R/expdata/p1") #set path to location of dataset
 household_power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 household_power_consumption$Date = as.Date(household_power_consumption$Date, "%d/%m/%Y")
 two_day_subset <- subset(household_power_consumption, household_power_consumption$Date == "2007-02-01" | household_power_consumption$Date == "2007-02-02")
--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+#setwd(<path-to-dataset) #set path to location of data
 household_power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 household_power_consumption$Date = as.Date(household_power_consumption$Date, "%d/%m/%Y")
 two_day_subset <- subset(household_power_consumption, household_power_consumption$Date == "2007-02-01" | household_power_consumption$Date == "2007-02-02")
